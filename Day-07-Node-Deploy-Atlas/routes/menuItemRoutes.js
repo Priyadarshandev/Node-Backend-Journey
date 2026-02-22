e59ce101f65ac2c0d3a644bfc34b58b764914a4e
Priyadarshan Kumar <priyadarshanwork24@gmail.com>
2026-02-22 13:13:30 +0530

    Test

---
+++ Day-07-Node-Deploy-Atlas/routes/menuItemRoutes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const MenuItem = require('./../models/MenuItem');
+
+//Post Method to add a Menu Item
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    const newMenu = new MenuItem(data);
+    const response = await newMenu.save();
+    console.log("data saved");
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//GET method to get the Menu Item
+router.get("/", async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    console.log("data fetched");
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/:tasteType", async (req, res) => {
+  try {
+    const tasteType = req.params.tasteType; // Extract the taste type your the URL Parameter
+    if (tasteType == "sweet" || tasteType == "spicy" || tasteType == "sour") {
+      const response = await MenuItem.find({ taste: tasteType});
+      console.log("response fetched");
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({ error: "Invalid taste type" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
